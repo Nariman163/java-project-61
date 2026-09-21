88eeb5e92542c101493ac1eb97ffa892d1bc1092
Nariman <nariman@MacBook-Pro-Nariman.local>
2026-09-21 23:35:53 +0300

    Fix project Engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -7,23 +7,24 @@ public class Engine {
 
     public static void runGame(String[] questions, String[] correctAnswers, String gameRule) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!\nMay I have your name?");
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
         String userName = scanner.nextLine();
         System.out.println("Hello, " + userName + "!");
         System.out.println(gameRule);
 
         for (int i = 0; i < ROUNDS; i++) {
             System.out.println("Question: " + questions[i]);
-            System.out.println("You answer: ");
+            System.out.print("Your choice: ");
             String userAnswer = scanner.nextLine();
             if (userAnswer.equals(correctAnswers[i])) {
                 System.out.println("Correct!");
             } else {
-                System.out.println(userAnswer + " is wrong answer ;(. Correct answer was " + correctAnswers[i]);
-                System.out.println("Let's try again, " + userName);
+                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + correctAnswers[i] + "'");
+                System.out.println("Let's try again, " + userName + "!");
                 return;
             }
         }
-        System.out.println("Congratulations, " +  userName + " !");
+        System.out.println("Congratulations, " +  userName + "!");
     }
 }
